694c148b3d092651a4be79ee23c6ec0def7adfb1
Leandro Arakaki <leanarakaki@gmail.com>
2024-01-14 22:07:25 -0400

    BCryptPasswordEncoder password

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/config/SecurityConfig.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -2,11 +2,18 @@ package com.devsuperior.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder gtePasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
--- password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$hrawX5G8a67AvlGb.4ba0OoEzsR07aLtHq4CZ/rUpf.QxnJx/bpP.');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$hrawX5G8a67AvlGb.4ba0OoEzsR07aLtHq4CZ/rUpf.QxnJx/bpP.');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
